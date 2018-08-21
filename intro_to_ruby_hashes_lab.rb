c217479f8f55a0e69d25c30526ac684c1d8fb311
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-21 22:59:28 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,8 +26,18 @@ def monopoly_with_third_tier
 	monopoly = {
   	  :railroads => {
   	    :pieces => 4, 
-  	    :names => {},
-  	    :rent_in_dollars => {}
+  	    :names => {
+  	      :reading_railroad => {},
+  	      :pennsylvania_railroad => {},
+  	      :b_and_o_railroad => {},
+  	      :shortline => {}
+  	    },
+  	    :rent_in_dollars => {
+  	      :one_piece_owned => 25,
+  	      :two_piece_owned => 50,
+  	      :three_piece_owned => 100,
+  	      :four_piece_owned => 200
+  	    }
   	  }
   	}
 end
