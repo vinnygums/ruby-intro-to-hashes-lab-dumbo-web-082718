39a0fefd0d0e8c1070f2e9f52c2aea9a1176f1fe
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-21 22:53:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,11 +30,8 @@ def monopoly_with_third_tier
   	    :rent_in_dollars => {
   	      :one_piece_owned => 25
   	    }
-  	  },
-  	  :rent_in_dollars => {
-  	    :one_piece_owned => 25
   	  }
-  }
+  	}
 end
 
 def monopoly_with_fourth_tier
