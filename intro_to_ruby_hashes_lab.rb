9ed4f9174815197ef90e89e84d697c09f423636f
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-21 23:24:43 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,7 +48,7 @@ def monopoly_with_fourth_tier
   	  :railroads => {
   	    :pieces => 4,
   	    :names => {
-  	      :reading_railroad => {"mortage_value" => "$100"},
+  	      :reading_railroad => {"mortgage_value" => "$100"},
   	      :pennsylvania_railroad => {},
   	      :b_and_o_railroad => {},
   	      :shortline => {}
