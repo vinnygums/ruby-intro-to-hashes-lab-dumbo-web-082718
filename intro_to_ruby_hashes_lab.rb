821acef6e4554ff5935d500999c41778323de0f2
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-21 23:11:38 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
   	      :shortline => {}
   	    },
   	    :rent_in_dollars => {
-  	      :one_piece_owned => 50
+  	      :one_piece_owned => 25
   	      :two_piece_owned => 50
   	      :three_piece_owned => 100
   	      :four_piece_owned => 200
